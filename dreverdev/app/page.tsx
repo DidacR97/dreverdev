78162a3d94d84276a7cbd6e13b4c99d9bcc88350
DidacR97 <didacr97@gmail.com>
2023-08-14 21:17:24 +0200

    Fix - Page.tsx error

--- dreverdev/app/page.tsx
+++ dreverdev/app/page.tsx
@@ -5,7 +5,6 @@ import { Code } from "@nextui-org/code"
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 
 export default function Home() {
 	return (
