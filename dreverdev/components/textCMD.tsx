f140d3cbb469a944fe923dc6f29ac184ada7d9fd
DidacR97 <didacr97@gmail.com>
2023-08-14 22:58:49 +0200

    feat - Developing about me section

---
+++ dreverdev/components/textCMD.tsx
@@ -0,0 +1,10 @@
+export default function TextCMD(text: {text: string}) {
+	return (
+		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+			 <h2>Beyond the keyboard</h2>
+             <span>Soy</span>
+             <span>Dídac Reverte</span>
+             <span> ,desarrollador Fullstack apasionado por la tecnología. Especializado en JavaScript y sus bibliotecas, como React y Next.js, también cuento con experiencia en lenguajes como Java, Python y PHP. Siempre estoy en la búsqueda de crear soluciones web intuitivas y de alto rendimiento, utilizando bases de datos como MongoDB y MySQL, y estilizando con herramientas modernas como Tailwind y Sass. Si buscas un enfoque moderno y eficiente para tu próximo proyecto, ¡estás en el lugar correcto!</span>
+		</section>
+	);
+}
