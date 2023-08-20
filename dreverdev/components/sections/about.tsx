ca302e6e35b4b437c21eda58d91b563d96fc696d
DidacR97 <didacr97@gmail.com>
2023-08-20 04:24:19 +0200

    fix - Error using apostrophe

--- dreverdev/components/sections/about.tsx
+++ dreverdev/components/sections/about.tsx
@@ -1,18 +1,9 @@
 import Image from "next/image";
 
-const imageStyle = {
-    position: "absolute",
-    width: "auto",
-    height: "100%",
-    left: "50%",
-    top: "50%",
-    transform: "translate(-50%, -50%)",
-}
-
 export default function About() {
     return (
         <section id="about" className="gap-4 py-8 md:py-10 flex max-w-full h-screen items-center">
-            <p className="text-xl flex flex-wrap w-[75%]">I'm a web developer skilled in JavaScript, experienced with Node.js and Next.js. I excel at solving complex challenges and value both teamwork and autonomy. My inherent curiosity keeps me evolving in the dynamic world of web development.</p>
+            <p className="text-xl flex flex-wrap w-[75%]">I&apos;m a web developer skilled in JavaScript, experienced with Node.js and Next.js. I excel at solving complex challenges and value both teamwork and autonomy. My inherent curiosity keeps me evolving in the dynamic world of web development.</p>
             <div className="w-[300px] h-[300px] overflow-hidden relative rounded">
                 <Image
                     alt={`me`}
--- dreverdev/components/sections/presentation.tsx
+++ dreverdev/components/sections/presentation.tsx
@@ -1,14 +1,13 @@
-import { DmMono } from "@/app/layout";
 import BlindText from "../blindText";
 
 export default function Presentation() {
 
     return (
         <section id="presentation" className="gap-4 py-8 md:py-10 flex flex-col justify-center max-w-full h-screen">
-            <h3 className="mb-2">Hi!, I'm</h3>
+            <h3 className="mb-2">Hi!, I&apos;m</h3>
             <h2 className="text-3xl md:text-8xl text-blue-500 flex flex-wrap">Dídac Reverte.</h2>
             <BlindText />
-            <p className={`text-lg max-w-[50%] ${DmMono.className}`}>I'm a software developer based near Barcelona, Spain, specialized in building extraordinary web solutions.</p>
+            <p className={`text-lg max-w-[50%]`}>I&apos;m a software developer based near Barcelona, Spain, specialized in building extraordinary web solutions.</p>
         </section>
     );
 }
