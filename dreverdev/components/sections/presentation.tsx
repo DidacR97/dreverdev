20b8a6fb013ce88faf797a34f6bcdb7dd1821b0e
Dídac Reverte <didacr97@gmail.com>
2023-08-23 08:00:08 +0200

    fix: Fix presentation section height

--- dreverdev/components/sections/presentation.tsx
+++ dreverdev/components/sections/presentation.tsx
@@ -3,7 +3,7 @@ import BlindText from "../blindText";
 export default function Presentation() {
 
     return (
-        <section id="presentation" className="gap-4 py-8 md:py-10 flex flex-col justify-center max-w-full h-screen">
+        <section id="presentation" className="gap-4 py-8 md:py-10 flex flex-col justify-center max-w-full presentation">
             <h3 className="mb-2">Hi!, I&apos;m</h3>
             <h2 className="text-5xl md:text-8xl text-blue-500 flex flex-wrap">Dídac Reverte.</h2>
             <BlindText />
