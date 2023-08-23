e734ba42a582d308bfce977c7431e51f221c2066
Dídac Reverte <37537956+DreverDev@users.noreply.github.com>
2023-08-23 16:29:42 +0200

    fix: Delete coma in presentation after Hi!

--- dreverdev/components/sections/presentation.tsx
+++ dreverdev/components/sections/presentation.tsx
@@ -4,7 +4,7 @@ export default function Presentation() {
 
     return (
         <section id="presentation" className="gap-4 py-8 md:py-10 flex flex-col justify-center max-w-full presentation">
-            <h3 className="mb-2">Hi!, I&apos;m</h3>
+            <h3 className="mb-2">Hi! I&apos;m</h3>
             <h2 className="text-5xl md:text-8xl text-blue-500 flex flex-wrap">Dídac Reverte.</h2>
             <BlindText />
             <p className={`text-lg md:max-w-[50%]`}>I&apos;m a software developer based near Barcelona, Spain, specialized in building extraordinary web solutions.</p>
